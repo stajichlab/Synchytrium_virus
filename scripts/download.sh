398e81220940f6ffc16f2e841b76a9e3c4d0ed66
Jason Stajich <jason.stajich@ucr.edu>
2021-12-03 15:32:28 -0800

    Update download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 #SBATCH -p short -N 1 -n 96 -C xeon --mem 64gb 
-module unload perl
+module unload miniconda2 miniconda3 perl
+module load parallel
 module load sratoolkit
 module load parallel-fastq-dump
 module load aspera
